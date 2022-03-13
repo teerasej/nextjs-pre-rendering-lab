4948e0bdf3f6eb6724b81428955834e4bf59c34f
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-13 19:44:10 +0700

    rename

---
+++ pages/StaticGenerationPost.tsx
@@ -0,0 +1,32 @@
+
+import { GetStaticProps, NextPage, InferGetStaticPropsType } from 'next'
+import React from 'react'
+
+
+type Props = {
+  username:string,
+  members:string[]
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  
+  return {
+    props: {
+      username: 'tody',
+      members:  ['Tony', 'Peter']
+    }
+  }
+}
+
+
+const StaticGenerationPost: NextPage = ({ username,members }: InferGetStaticPropsType<typeof getStaticProps>) => {
+
+  console.log(username)
+  console.log(members?.[0])
+
+  return (
+    <div>1. {username} with {members?.length} members</div>
+  )
+}
+
+export default StaticGenerationPost
