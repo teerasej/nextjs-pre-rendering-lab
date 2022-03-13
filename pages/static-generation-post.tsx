9f6289fc876cdd096465a0fb43dd5395bf374504
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-13 16:41:49 +0700

    create page with static generation - without value (props)

---
+++ pages/static-generation-post.tsx
@@ -0,0 +1,12 @@
+
+import { NextPage } from 'next'
+import React from 'react'
+
+
+const StaticGenerationPost: NextPage = () => {
+  return (
+    <div>StaticGenerationPost</div>
+  )
+}
+
+export default StaticGenerationPost
