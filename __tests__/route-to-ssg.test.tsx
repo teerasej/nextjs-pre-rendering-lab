a9b5228fa1f41b51ba81d2369f344f7e63cd3ce0
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-04-04 21:06:53 +0700

    use serverRender to save snapshot

--- __tests__/route-to-ssg.test.tsx
+++ __tests__/route-to-ssg.test.tsx
@@ -4,14 +4,21 @@ import '@testing-library/jest-dom'
 
 test('Home Page', async () => {
   
-    const { render } = await getPage({
-      route: '/',
+    const page = await getPage({
+      route: '/StaticGenerationPost'
     })
 
-    render()
-    expect(screen.getByText('Hello')).toBeInTheDocument()
+    page.render()
 
-    fireEvent.click(screen.getByRole('link'))
-    expect(await screen.findByText('SSG')).toBeInTheDocument()
-    expect(await screen.findByText('1. tody with 2 members')).toBeInTheDocument()
+    const { html } = page.serverRenderToString();
+    expect(html).toMatchSnapshot()
+
+    // //expect(screen.getByText('Hello')).toBeInTheDocument()
+
+    // expect(page.render().nextRoot).toMatchSnapshot()
+
+    // screen.findAllByAltText
+    // fireEvent.click(screen.getByRole('link'))
+    // expect(await screen.findByText('SSG')).toBeInTheDocument()
+    // expect(await screen.findByText('1. tody with 2 members')).toBeInTheDocument()
 });
