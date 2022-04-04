cd99e400bbd70954e5a1b1ce7e362d8adf97cdd3
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-04-04 13:10:38 +0700

    create route to ssq

--- __tests__/route-to-ssg.test.tsx
+++ __tests__/route-to-ssg.test.tsx
@@ -1,17 +1,17 @@
 import { getPage } from 'next-page-tester';
 import { screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
-import { link } from 'fs';
 
 test('Home Page', async () => {
   
     const { render } = await getPage({
       route: '/',
-    });
+    })
 
-    render();
-    expect(screen.getByText('Hello')).toBeInTheDocument();
+    render()
+    expect(screen.getByText('Hello')).toBeInTheDocument()
 
-    fireEvent.click(screen.getByRole('link'));
-    expect(await screen.findByText('SSG')).toBeInTheDocument();
+    fireEvent.click(screen.getByRole('link'))
+    expect(await screen.findByText('SSG')).toBeInTheDocument()
+    expect(await screen.findByText('1. tody with 2 members')).toBeInTheDocument()
 });
