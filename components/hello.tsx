fc52050a2182bce5dc5d70839698959c8aa0566a
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-04-05 08:34:42 +0700

    add func in hello component so it cannot be coverage

--- components/hello.tsx
+++ components/hello.tsx
@@ -1,9 +1,20 @@
 import { NextComponentType } from "next";
+import { useState } from "react";
 
 const Hello = () => {
+
+    const [sayWorld, setSayWorld] = useState(false)
+
+    const doWorld = () => {
+        setSayWorld(true)
+    }
+
+
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Hello {
+                    sayWorld ?? <span>World</span>
+                }</h1>
         </div>
     )
 }
